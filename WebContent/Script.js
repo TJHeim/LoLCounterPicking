144059fb490fe3bbda5ed6a538ea7d99e425e0bc
TJHeim <TJHeim@users.noreply.github.com>
2018-07-12 20:52:24 -0500

    Made a function to place all blocks

--- WebContent/Script.js
+++ WebContent/Script.js
@@ -1,8 +1,61 @@
-function setToYellow()
+function onStart()
 {
-	$(document).ready(function(){
-		$("p").click(function(){
-			$(this).css("background-color", "yellow");
-		})
-	})
+	const champsPerTeam=5;
+	const teamArray=["ally", "enemy"];
+	const teamArrayUppercased=["Ally", "Enemy"];
+	const spaceBetweenChampAndPosBlocks=10;
+	const spaceBetweenChampBlocks=30;
+	
+	for(var atChampBlock=1; atChampBlock<=champsPerTeam; atChampBlock++)
+	{
+		/*Create an allyChamp and an enemyChamp block*/
+		for(var team=0; team<teamArray.length; team++)
+		{
+			/*Initiate the p and select elements*/
+			var champBlock=document.createElement("p");
+			var champSelect=document.createElement("select");
+			
+			/*Set the id and class of all elements*/
+			var teamName=teamArray[team];
+			champBlock.setAttribute("id", teamName+"ChampBlock"+atChampBlock);
+			champBlock.setAttribute("class", teamName+"Block");
+			champSelect.setAttribute("id", teamName+"ChampSelect"+atChampBlock);
+			champSelect.setAttribute("class", teamName+"ChampSelect");
+			
+			/*Set the position of the block*/
+			champBlock.setAttribute("style", "top:"+(spaceBetweenChampAndPosBlocks*(atChampBlock-1)+spaceBetweenChampBlocks*(atChampBlock-1))+"px")
+			
+			/*Append the contents of the block*/
+			champBlock.innerHTML=teamArrayUppercased[team]+" Champion: ";
+			champBlock.appendChild(champSelect);
+			
+			/*Append the block to the body*/
+			document.body.appendChild(champBlock);
+		}
+		
+		/*Create an allyPos and an enemyPos block*/
+		for(var team=0; team<teamArray.length; team++)
+		{
+			/*Initiate the p and select elements*/
+			var posBlock=document.createElement("p");
+			var posSelect=document.createElement("select");
+			
+			/*Set the id and class of all elements*/
+			var teamName=teamArray[team];
+			posBlock.setAttribute("id", teamName+"PosBlock"+atChampBlock);
+			posBlock.setAttribute("class", teamName+"Block");
+			posSelect.setAttribute("id", teamName+"PosSelect"+atChampBlock);
+			posSelect.setAttribute("class", teamName+"PosSelect");
+			
+			/*Set the position of the block*/
+			posBlock.setAttribute("style", "top:"+(spaceBetweenChampAndPosBlocks*atChampBlock+spaceBetweenChampBlocks*(atChampBlock-1))+"px")
+			
+			/*Append the contents of the block*/
+			posBlock.innerHTML=teamArrayUppercased[team]+" Position: ";
+			posBlock.appendChild(posSelect);
+			
+			/*Append the block to the body*/
+			document.body.appendChild(posBlock);
+		}
+	}
 }
