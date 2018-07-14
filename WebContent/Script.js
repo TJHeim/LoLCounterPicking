6778acc0da0cb52cb89b9cf29e2638c8ca6dd049
TJHeim <TJHeim@users.noreply.github.com>
2018-07-14 12:37:18 -0500

    Added the collectData function

--- WebContent/Script.js
+++ WebContent/Script.js
@@ -6,7 +6,7 @@
 //});
 
 const champList=["Aatrox", "Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Ashe", "Aurelion Sol", "Azir", "Bard", "Blitzcrank", "Brand", "Braum", "Caitlyn", "Camille", "Cassiopeia", "Cho"+"'"+"gath", "Corki", "Darius", "Diana", "Dr. Mundo", "Draven", "Ekko", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Hecarim", "Heimerdinger", "Illaoi", "Irelia", "Ivern", "Janna", "Jarvan IV", "Jax", "Jayce", "Jhin", "Jinx", "Kai"+"'"+"Sa", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kayn", "Kennen", "Kha"+"'"+"Zix", "Kindred", "Kled", "Kog"+"'"+"Maw", "LeBlanc", "Lee Sin", "Leona", "Lissandra", "Lucian", "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "Master Yi", "Miss Fortune", "Mordekaiser", "Morgana", "Nami", "Nasus", "Nautilus", "Nidalee", "Nocturne", "Nunu", "Olaf", "Orianna", "Ornn", "Pantheon", "Poppy", "Pyke", "Quinn", "Rakan", "Rammus", "Rek"+"'"+"Sai", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Sejuani", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Syndra", "Tahm Kench", "Taliyah", "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "Twisted Fate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Vel"+"'"+"Koz", "Vi", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong", "Xayah", "Xerath", "Xin Zhao", "Yasuo", "Yorick", "Zac", "Zed", "Ziggs", "Zilean", "Zoe", "Zyra"];
-const champAccessList=["aatrox", "ahri", "akali", "alistar", "amumu", "anivia", "annie", "ashe", "aurelion-sol", "azir", "bard", "blitzcrank", "brand", "braum", "caitlyn", "camille", "cassiopeia", "chogath", "corki", "darius", "diana", "dr-mundo", "draven", "ekko", "elise", "evelynn", "ezreal", "fiddlesticks", "fiora", "fizz", "galio", "gangplank", "garen", "gnar", "gragas", "graves", "hecarim", "heimerdinger", "illaoi", "irelia", "ivern", "janna", "jarvan-iv", "jax", "jayce", "jhin", "jinx", "kaisa", "kalista", "karma", "karthus", "kassadin", "katarina", "kayle", "kayn", "kennen", "khazix", "kindred", "kled", "kogmaw", "leblanc", "lee-sin", "leona", "lissandra", "lucian", "lulu", "lux", "malphite", "malzahar", "maokai", "master-yi", "miss-fortune", "mordekaiser", "morgana", "nami", "nasus", "nautilus", "nidalee", "nocturne", "nunu", "olaf", "orianna", "ornn", "pantheon", "poppy", "pyke", "quinn", "rakan", "rammus", "reksai", "renekton", "rengar", "riven", "rumble", "ryze", "sejuani", "shaco", "shen", "shyvana", "singed", "sion", "sivir", "skarner", "sona", "soraka", "swain", "syndra", "tahm-kench", "taliyah", "talon", "taric", "teemo", "thresh", "tristana", "trundle", "tryndamere", "twisted-fate", "twitch", "udyr", "urgot", "varus", "vayne", "veigar", "velkoz", "vi", "viktor", "vladimir", "volibear", "warwick", "wukong", "xayah", "xerath", "xin-zhao", "yasuo", "yorick", "zac", "zed", "ziggs", "zilean", "zoe", "zyra"];
+const champAccessList=["aatrox", "ahri", "akali", "alistar", "amumu", "anivia", "annie", "ashe", "aurelionsol", "azir", "bard", "blitzcrank", "brand", "braum", "caitlyn", "camille", "cassiopeia", "chogath", "corki", "darius", "diana", "drmundo", "draven", "ekko", "elise", "evelynn", "ezreal", "fiddlesticks", "fiora", "fizz", "galio", "gangplank", "garen", "gnar", "gragas", "graves", "hecarim", "heimerdinger", "illaoi", "irelia", "ivern", "janna", "jarvaniv", "jax", "jayce", "jhin", "jinx", "kaisa", "kalista", "karma", "karthus", "kassadin", "katarina", "kayle", "kayn", "kennen", "khazix", "kindred", "kled", "kogmaw", "leblanc", "lee-sin", "leona", "lissandra", "lucian", "lulu", "lux", "malphite", "malzahar", "maokai", "masteryi", "missfortune", "mordekaiser", "morgana", "nami", "nasus", "nautilus", "nidalee", "nocturne", "nunu", "olaf", "orianna", "ornn", "pantheon", "poppy", "pyke", "quinn", "rakan", "rammus", "reksai", "renekton", "rengar", "riven", "rumble", "ryze", "sejuani", "shaco", "shen", "shyvana", "singed", "sion", "sivir", "skarner", "sona", "soraka", "swain", "syndra", "tahmkench", "taliyah", "talon", "taric", "teemo", "thresh", "tristana", "trundle", "tryndamere", "twistedfate", "twitch", "udyr", "urgot", "varus", "vayne", "veigar", "velkoz", "vi", "viktor", "vladimir", "volibear", "warwick", "wukong", "xayah", "xerath", "xinzhao", "yasuo", "yorick", "zac", "zed", "ziggs", "zilean", "zoe", "zyra"];
 const champPosList=["", "Top", "Mid", "Bot", "Sup", "Jg"]
 const champPosAccessList=["general", "top", "mid", "bottom", "bottom", "jungle"]
 
@@ -78,11 +78,108 @@ function onBodyLoad()
 
 function refreshCounterSource()
 {
+	var compareTypeList=["Strong", "Weak", "Even", "Well"];
+	var compareTypeAccessList=["strong", "weak", "even", "good"];
 	for(var champ=0; champ<champAccessList.length; champ++)
 	{
-		
+		for(var compareType=0; compareType<compareTypeAccessList.length-2; compareType++)
+		{
+			for(var champPos=0; champPos<champPosAccessList.length; champPos++)
+			{
+				var activeChamp=champAccessList[champ];
+				var activeCompareType=compareTypeAccessList[compareType];
+				var activeChampPos=champPosAccessList[champPos];
+				
+				$.get("https://allorigins.me/get?method=raw&url="+encodeURIComponent("https://lolcounter.com/champions/"+activeChamp+"/"+activeCompareType+"/"+activeChampPos), collectDataAndSaveFile(html, activeChamp, activeCompareType, activeChampPos));
+			}
+		}
 	}
-	$.get('https://allorigins.me/get?method=raw&url=' + encodeURIComponent('https://lolcounter.com/champions/zed/weak'), function(data){
-	    console.log(data);
-	});
+	
+}
+
+
+
+
+function collectDataAndSaveFile(html, activeChamp, activeCompareType, activeChampPos)
+{
+	var data=collectData(html);
+}
+
+
+
+
+function collectData(html)
+{
+	/*String to contain the collected data*/
+	var data="";
+	
+	/*Find the start and end of the data and trim the html*/
+	var startOfData=html.indexOf("<div class='weak-block full'");
+	//var endOfData=findEndOfDiv(html, startOfData, 1);
+	html=html.substring(startOfData, /*endOfData*/).replace(/\s/g, "");
+	
+	/*Run through the html to find the data and give it to the data var*/
+	var activePos=0;
+	while(activePos<html.length)
+	{
+		/*Find the start of the champion's name and break the loop if there are no more*/
+		var startOfChampName=html.indexOf('find=', activePos);
+		if(startOfChampName==-1)
+			break;
+		else
+			startOfChampName=startOfChampName+'find="'.length;
+		
+		/*Find the end of the champion's name*/
+		var endOfChampName=html.indexOf("'", startOfChampName);
+		activePos=endOfChampName;
+		
+		/*Get name and add it to the data*/
+		var champAccessName=html.substring(startOfChampName, endOfChampName);
+		data=data.concat(champAccessName);
+		
+		/*Find the start of the champion's per*/
+		var startOfChampPer=html.indexOf("width:", activePos)+"width:".length;
+		
+		/*Find the end of the champion's per*/
+		var endOfChampPer=html.indexOf("%", startOfChampPer);
+		activePos=endOfChampPer;
+		
+		/*Get per and add it to the data*/
+		var champPer=html.substring(startOfChampPer, endOfChampPer);
+		data=data.concat(champPer);
+	}
+	
+	return data;
+}
+
+
+
+function findEndOfDiv(html, pos, numberOfDivsInside)
+{
+	if(numberOfDivsInside==0)
+		return pos;
+	
+	var indexOfNextDiv=html.indexOf("div", pos);
+	
+	if(html.substring(indexOfNextDiv-1, indexOfNextDiv)=="<")
+		return findEndOfDiv(html, indexOfNextDiv+"div>".length, numberOfDivsInside+1);
+	
+	else //if(html.substring(indexOfNextDiv-1, indexOfNextDiv)=="/")
+		return findEndOfDiv(html, indexOfNextDiv+"div>".length, numberOfDivsInside-1)
+}
+
+
+
+
+function binarySearch(array, champ, low, high)
+{
+  var mid=Math.floor((low+high)/2);
+  if(low>high)
+    return -1;
+  else if(array[mid]==champ)
+    return mid;
+  else if(array[mid]>champ)
+    return binarySearch(array, champ, low, mid-1);
+  else if(array[mid]<champ)
+    return binarySearch(array, champ, mid+1, high);
 }
