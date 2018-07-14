514c90af19d4a4ac2948893072ec413fd9cd699d
TJHeim <TJHeim@users.noreply.github.com>
2018-07-13 19:25:15 -0500

    Added champ lists

--- WebContent/Script.js
+++ WebContent/Script.js
@@ -1,10 +1,21 @@
+//$("#btn-save").click( function() {
+//  var text = $("#textarea").val();
+//  var filename = $("#input-fileName").val()
+//  var blob = new Blob([text], {type: "text/plain;charset=utf-8"});
+//  saveAs(blob, filename+".txt");
+//});
+
+const champList=["Aatrox", "Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Ashe", "Aurelion Sol", "Azir", "Bard", "Blitzcrank", "Brand", "Braum", "Caitlyn", "Camille", "Cassiopeia", "Cho"+"'"+"gath", "Corki", "Darius", "Diana", "Dr. Mundo", "Draven", "Ekko", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Hecarim", "Heimerdinger", "Illaoi", "Irelia", "Ivern", "Janna", "Jarvan IV", "Jax", "Jayce", "Jhin", "Jinx", "Kai"+"'"+"Sa", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kayn", "Kennen", "Kha"+"'"+"Zix", "Kindred", "Kled", "Kog"+"'"+"Maw", "LeBlanc", "Lee Sin", "Leona", "Lissandra", "Lucian", "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "Master Yi", "Miss Fortune", "Mordekaiser", "Morgana", "Nami", "Nasus", "Nautilus", "Nidalee", "Nocturne", "Nunu", "Olaf", "Orianna", "Ornn", "Pantheon", "Poppy", "Pyke", "Quinn", "Rakan", "Rammus", "Rek"+"'"+"Sai", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Sejuani", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Syndra", "Tahm Kench", "Taliyah", "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "Twisted Fate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Vel"+"'"+"Koz", "Vi", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong", "Xayah", "Xerath", "Xin Zhao", "Yasuo", "Yorick", "Zac", "Zed", "Ziggs", "Zilean", "Zoe", "Zyra"];
+const champAccessList=["aatrox", "ahri", "akali", "alistar", "amumu", "anivia", "annie", "ashe", "aurelion-sol", "azir", "bard", "blitzcrank", "brand", "braum", "caitlyn", "camille", "cassiopeia", "chogath", "corki", "darius", "diana", "dr-mundo", "draven", "ekko", "elise", "evelynn", "ezreal", "fiddlesticks", "fiora", "fizz", "galio", "gangplank", "garen", "gnar", "gragas", "graves", "hecarim", "heimerdinger", "illaoi", "irelia", "ivern", "janna", "jarvan-iv", "jax", "jayce", "jhin", "jinx", "kaisa", "kalista", "karma", "karthus", "kassadin", "katarina", "kayle", "kayn", "kennen", "khazix", "kindred", "kled", "kogmaw", "leblanc", "lee-sin", "leona", "lissandra", "lucian", "lulu", "lux", "malphite", "malzahar", "maokai", "master-yi", "miss-fortune", "mordekaiser", "morgana", "nami", "nasus", "nautilus", "nidalee", "nocturne", "nunu", "olaf", "orianna", "ornn", "pantheon", "poppy", "pyke", "quinn", "rakan", "rammus", "reksai", "renekton", "rengar", "riven", "rumble", "ryze", "sejuani", "shaco", "shen", "shyvana", "singed", "sion", "sivir", "skarner", "sona", "soraka", "swain", "syndra", "tahm-kench", "taliyah", "talon", "taric", "teemo", "thresh", "tristana", "trundle", "tryndamere", "twisted-fate", "twitch", "udyr", "urgot", "varus", "vayne", "veigar", "velkoz", "vi", "viktor", "vladimir", "volibear", "warwick", "wukong", "xayah", "xerath", "xin-zhao", "yasuo", "yorick", "zac", "zed", "ziggs", "zilean", "zoe", "zyra"];
+
 function onBodyLoad()
 {
 	const champsPerTeam=5;
 	const teamArray=["ally", "enemy"];
 	const teamArrayUppercased=["Ally", "Enemy"];
-	const spaceBetweenChampAndPosBlocks=25;
-	const spaceBetweenChampBlocks=70;
+	const pixelsBeforeStartofBlocks=40;
+	const pixelsBetweenChampAndPosBlocks=25;
+	const pixelsBetweenChampBlocks=70;
 	
 	for(var atChampBlock=1; atChampBlock<=champsPerTeam; atChampBlock++)
 	{
@@ -23,7 +34,7 @@ function onBodyLoad()
 			champSelect.setAttribute("class", teamName+"ChampSelect");
 			
 			/*Set the position of the block*/
-			champBlock.setAttribute("style", "top:"+(spaceBetweenChampAndPosBlocks*(atChampBlock-1)+spaceBetweenChampBlocks*(atChampBlock-1))+"px")
+			champBlock.setAttribute("style", "top:"+(pixelsBeforeStartofBlocks+pixelsBetweenChampAndPosBlocks*(atChampBlock-1)+pixelsBetweenChampBlocks*(atChampBlock-1))+"px")
 			
 			/*Append the contents of the block*/
 			champBlock.innerHTML=teamArrayUppercased[team]+" Champion: ";
@@ -48,7 +59,7 @@ function onBodyLoad()
 			posSelect.setAttribute("class", teamName+"PosSelect");
 			
 			/*Set the position of the block*/
-			posBlock.setAttribute("style", "top:"+(spaceBetweenChampAndPosBlocks*atChampBlock+spaceBetweenChampBlocks*(atChampBlock-1))+"px")
+			posBlock.setAttribute("style", "top:"+(pixelsBeforeStartofBlocks+pixelsBetweenChampAndPosBlocks*atChampBlock+pixelsBetweenChampBlocks*(atChampBlock-1))+"px")
 			
 			/*Append the contents of the block*/
 			posBlock.innerHTML=teamArrayUppercased[team]+" Position: ";
